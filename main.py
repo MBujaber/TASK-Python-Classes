a6447a79eef419212f69d7f5f64a62199cc1423e
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-09-28 20:47:16 +0300

    solve

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 class Wallet:
     def __init__(self, money):
-        pass
+        # money = 0
+        self.money = money
+        
 
-    def credit(amount):
-        pass
 
-    def debit(amount):
-        pass
+    def credit(self, amount):
+        self.money = amount + self.money
+        print(f"you have money {self.money}")
+
+    def debit(self, amount):
+        self.money = self.money - amount
+        print(f"you have money {self.money}")
 
 
 wallet = Wallet(6)
-wallet = Wallet()  # This should default money inside the wallet to 0
+# wallet = Wallet()  # This should default money inside the wallet to 0
 print(wallet)
+# print(wallet.money)
+# wallet.credit(4)
+# wallet.debit(5)
 
 
 class Person:
-    # Implement the code here
-    pass
+    def __init__(self, name, location, money):
+        self.name = name
+        self.location = location
+        self. wallet = Wallet(money)
+    
 
 
 person = Person("Moh", 5, 50)
+print(person.wallet.money)
 
 
-class Vendor:
-    # implement Vendor!
-    pass
+# class Vendor:
+#     # implement Vendor!
+#     pass
 
 
-vendor = Vendor("Abdallah", 3, 6)
+# vendor = Vendor("Abdallah", 3, 6)
 
 
-class Customer:
-    # implement Customer!
-    pass
+# class Customer:
+#     # implement Customer!
+#     pass
 
 
-customer = Customer("Abdallah", 3, 6)
+# customer = Customer("Abdallah", 3, 6)
